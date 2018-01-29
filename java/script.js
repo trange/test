8f66690abe77c297dac4aaa401811106d1fed37e
trange <range.t.range@gmail.com>
2018-01-29 21:53:19 +0900

    var-5

--- java/script.js
+++ java/script.js
@@ -1,10 +1,5 @@
-var test1=80;
-var test2=70;
-
-if(test1>=80){
-    if(test2<=89){
-        document.write("不合格");
-    }else if(test2>=90){
-        document.write("合格");
-    }
+for(var i=18;i<=65;i++){
+    document.write("<option value='z'>);
+    document.write(i);
+    document.write("</option>");
 }
