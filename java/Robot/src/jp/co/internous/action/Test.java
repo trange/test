b4a877eeb823351a57ac49b7d64eef439fe3a1d3
trange <range.t.range@gmail.com>
2018-02-21 21:40:36 +0900

    java-1

---
+++ java/Robot/src/jp/co/internous/action/Test.java
@@ -0,0 +1,44 @@
+/**
+ *
+ */
+package jp.co.internous.action;
+
+/**
+ * @author testuser
+ *
+ */
+public class Test {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+
+		Robot aibo = new Robot();
+		aibo.name="aibo";
+		aibo.talk();
+		aibo.walk();
+		aibo.run();
+
+		Robot asimo = new Robot();
+		asimo.name="asimo";
+		asimo.talk();
+		asimo.walk();
+		asimo.run();
+
+		Robot pepper = new Robot();
+		pepper.name="pepper";
+		pepper.talk();
+		pepper.walk();
+		pepper.run();
+
+		Robot doraemon = new Robot();
+		doraemon.name="ドラえもん";
+		doraemon.talk();
+		doraemon.walk();
+		doraemon.run();
+
+	}
+
+}
