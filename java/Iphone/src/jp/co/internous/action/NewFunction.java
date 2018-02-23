7a7517509035e25a2510e4c85c264a68efd647b3
trange <range.t.range@gmail.com>
2018-02-23 21:42:13 +0900

    java-2

---
+++ java/Iphone/src/jp/co/internous/action/NewFunction.java
@@ -0,0 +1,10 @@
+package jp.co.internous.action;
+
+public interface NewFunction {
+
+	public abstract void call();
+	public abstract void mail();
+	public abstract void phone();
+	public abstract void internet();
+
+}
