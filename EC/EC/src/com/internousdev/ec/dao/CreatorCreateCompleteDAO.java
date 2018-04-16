0c59d22959fe65f82a26de06126a948a7f10177c
trange <range.t.range@gmail.com>
2018-04-16 20:59:47 +0900

    2

---
+++ EC/EC/src/com/internousdev/ec/dao/CreatorCreateCompleteDAO.java
@@ -0,0 +1,30 @@
+package com.internousdev.ec.dao;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
+import com.internousdev.ec.util.DBConnector;
+import com.internousdev.ec.util.DateUtil;
+
+public class CreatorCreateCompleteDAO {
+	private DBConnector dbConnector=new DBConnector();
+	private Connection connection=dbConnector.getConnection();
+	private DateUtil dateUtil=new DateUtil();
+	private String sql="INSERT INTO login_creator_transaction (login_id, login_pass, user_name, insert_date) VALUES(?, ?, ?, ?)";
+	public void createUser(String loginUserId, String loginUserPassword, String userName) throws SQLException{
+		try{
+			PreparedStatement preparedStatement=connection.prepareStatement(sql);
+			preparedStatement.setString(1, loginUserId);
+			preparedStatement.setString(2, loginUserPassword);
+			preparedStatement.setString(3, userName);
+			preparedStatement.setString(4, dateUtil.getDate());
+
+			preparedStatement.execute();
+		}catch(Exception e){
+			e.printStackTrace();
+		}finally{
+			connection.close();
+		}
+	}
+}
